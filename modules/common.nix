b0bc038f5c9c427d536ff886541c49d3cc16dc62
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-09 23:34:35 -0500

    Enable SSH service in Nix configuration

--- modules/common.nix
+++ modules/common.nix
@@ -4,6 +4,9 @@
   # Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];  
 
+  # Enable SSH
+  services.openssh.enable = true;
+
   # Include some basic packages
   environment.systemPackages = with pkgs; [
     just
