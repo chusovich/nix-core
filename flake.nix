e120b2a6e27478368ebcac818282f94d0b5f2e05
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-09 22:57:08 -0500

    Add flake.nix for reusable NixOS modules

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Reusable NixOS modules for servers";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    coreModules = {
+      # use default to import all
+      default = import ./modules;
+      
+      # or import each module individually
+      docker = import ./modules/docker.nix;
+      prometheus-exporters = import ./modules/prometheus-exporters.nix;
+      beszel = import ./modules/beszel.nix;
+    };
+  };
+}
