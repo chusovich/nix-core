c93a2d5f233b772ea3bb7b1e4b0362c960e269f5
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-02-25 13:01:59 -0500

    Clean up commented code in prometheus-exporters.nix
    Removed commented-out configuration for Prometheus exporters.

--- modules/prometheus-exporters.nix
+++ modules/prometheus-exporters.nix
@@ -13,25 +13,3 @@
     firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
   };
 }
-
-#{ config, lib, pkgs, ... }:
-#
-#let
-#  cfg = config.myModules.prometheusExporters;
-#in
-#{
-#  options.myModules.prometheusExporters = {
-#    enable = lib.mkEnableOption "Prometheus exporters";
-#
-#    node = lib.mkEnableOption "Node exporter";
-#  };
-#
-#  config = lib.mkIf cfg.enable {
-#    services.prometheus.exporters = {
-#      node = lib.mkIf cfg.node {
-#        enable = true;
-#        port = 9100;
-#      };
-#    };
-#  };
-#}
